98bf66e703b4fe71cc2904235a6543c21317cd5f
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-11-06 20:26:07 +0100

    +Added the free stuff

--- Program.cs
+++ Program.cs
@@ -2,10 +2,14 @@
 {
     internal class Program
     {
+        
+         
         static void Main(string[] args)
         {
+            Restaurant Johnnys = new Restaurant();
 
 
+            MenuItem Bolognese = new MenuItem(1, "Spaghetti Bolognese", 125m);
 
             Console.ReadLine();
         }
---
+++ Restaurant.cs
@@ -0,0 +1,16 @@
+﻿namespace LABB4_Generics_SUT24
+{
+    internal class Restaurant
+    {
+        private List<MenuItem> = _menuItem new List<MenuItem>();
+        private Queue<Order> = _orders new Queue<Order>();
+
+        
+        void AddtoMenu(MenuItem menuItem)
+        {
+            _menuItem.Add(menuItem);
+            Console.WriteLine($"{menuItem} was added to the menu.");
+        }
+
+    }
+}
