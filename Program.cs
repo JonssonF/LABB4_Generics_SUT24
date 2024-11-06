b80dbeab6efc9b3b4eafdcb7fe1f36c861416c49
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-11-06 13:44:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LABB4_Generics_SUT24 //Fredrik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
